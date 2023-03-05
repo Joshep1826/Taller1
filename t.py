6ed514d2f633cba7b47b38d21367224a3c78b56d
Joshep1826 <joshep.moreno@outlook.es>
2023-03-05 14:55:18 +0100

    Se adiciona taller

---
+++ t.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.google.com/")
+
+driver.set_window_size(1024, 600)
+driver.maximize_window()
+
+element_web = driver.find_element(By.NAME, "q")
+element_web.send_keys("willie colon che che cole itagüí" + Keys.ENTER)
+
+time.sleep(15)
